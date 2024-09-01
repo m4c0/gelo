c37fff56cb97412a8c6e8057efc37669bb67e64c
Eduardo Costa <m4c0@users.noreply.github.com>
2024-09-01 16:15:21 +0100

    uniform, blending, some of texture

--- poc.cpp
+++ poc.cpp
@@ -16,23 +16,33 @@ static constexpr const auto frag_shader = R"(
   void main() { gl_FragColor = vec4(0.1, 0.2, 0.3, 1.0); }
 )"_s;
 static constexpr const float quad[] { 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0 };
+static constexpr const unsigned text[] { 0xFF00FFFF }; 
 
 namespace gelo {
+  IMPORT(void, active_texture)(int id);
   IMPORT(void, attach_shader)(int prog, int shd);
   IMPORT(void, bind_buffer)(int type, int buf);
+  IMPORT(void, bind_texture)(int type, int txt);
+  IMPORT(void, blend_func)(int src, int dst);
   IMPORT(void, buffer_data)(int type, const void * ptr, unsigned sz, int mode);
   IMPORT(void, compile_shader)(int shd);
   IMPORT(int, create_buffer)();
   IMPORT(int, create_program)();
   IMPORT(int, create_shader)(int type);
+  IMPORT(int, create_texture)();
+  IMPORT(void, enable)(int feat);
   IMPORT(void, enable_vertex_attrib_array)(int idx);
   IMPORT(void, get_program_info_log)(int prog, char * buf, unsigned sz);
   IMPORT(bool, get_program_parameter_b)(int prog, int n);
   IMPORT(void, get_shader_info_log)(int shd, char * buf, unsigned sz);
   IMPORT(bool, get_shader_parameter_b)(int shd, int n);
+  IMPORT(int, get_uniform_location)(int prog, const char * name, unsigned sz);
   IMPORT(int, link_program)(int prog);
   IMPORT(void, shader_source)(int shd, const char * src, unsigned sz);
+  IMPORT(int, uniform2f)(int u, float, float);
+  IMPORT(int, uniform1i)(int u, int);
   IMPORT(int, use_program)(int prog);
+  IMPORT(void, tex_image_2d)(int t, int lvl, int i_fmt, int w, int h, int border, int fmt, int type, const void * data, int sz);
   IMPORT(void, vertex_attrib_array_pointer)(int idx, int qty, int type, bool norm, int stride, int offset);
 }
 
@@ -71,6 +81,14 @@ static void run() {
   buffer_data(ARRAY_BUFFER, quad, sizeof(quad), STATIC_DRAW);
   enable_vertex_attrib_array(0);
   vertex_attrib_array_pointer(0, 2, FLOAT, false, 0, 0);
+
+  enable(BLEND);
+  blend_func(ONE, ONE_MINUS_SRC_ALPHA);
+
+  auto t = create_texture();
+  active_texture(TEXTURE0);
+  bind_texture(TEXTURE_2D, t);
+  tex_image_2d(TEXTURE_2D, 0, RGBA, 1, 1, 0, RGBA, UNSIGNED_BYTE, text, sizeof(text));
 }
 
 struct init {
