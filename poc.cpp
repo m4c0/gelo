c3b4d4e5b72fa875f746780ac346cca5093728ea
Eduardo Costa <m4c0@users.noreply.github.com>
2024-09-01 16:30:18 +0100

    moving calls to module

--- poc.cpp
+++ poc.cpp
@@ -7,8 +7,6 @@ import vaselin;
 
 using namespace jute::literals;
 
-#define IMPORT(r, n) extern r __attribute__((import_module("gelo"), import_name(#n))) n
-
 static constexpr const auto vert_shader = R"(
   attribute vec2 pos;
   void main() { gl_Position = vec4(pos, 0, 1); }
@@ -18,39 +16,6 @@ static constexpr const auto frag_shader = R"(
 )"_s;
 static constexpr const float quad[] { 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0 };
 static constexpr const unsigned text[] { 0xFF00FFFF }; 
-
-namespace gelo {
-  IMPORT(void, active_texture)(int id);
-  IMPORT(void, attach_shader)(int prog, int shd);
-  IMPORT(void, bind_buffer)(int type, int buf);
-  IMPORT(void, bind_texture)(int type, int txt);
-  IMPORT(void, blend_func)(int src, int dst);
-  IMPORT(void, buffer_data)(int type, const void * ptr, unsigned sz, int mode);
-  IMPORT(void, clear)(int who);
-  IMPORT(void, clear_color)(float r, float g, float b, float a);
-  IMPORT(void, compile_shader)(int shd);
-  IMPORT(int, create_buffer)();
-  IMPORT(int, create_program)();
-  IMPORT(int, create_shader)(int type);
-  IMPORT(int, create_texture)();
-  IMPORT(void, draw_arrays_instanced)(int mode, int first, int v_count, int i_count);
-  IMPORT(void, enable)(int feat);
-  IMPORT(void, enable_vertex_attrib_array)(int idx);
-  IMPORT(void, get_program_info_log)(int prog, char * buf, unsigned sz);
-  IMPORT(bool, get_program_parameter_b)(int prog, int n);
-  IMPORT(void, get_shader_info_log)(int shd, char * buf, unsigned sz);
-  IMPORT(bool, get_shader_parameter_b)(int shd, int n);
-  IMPORT(int, get_uniform_location)(int prog, const char * name, unsigned sz);
-  IMPORT(int, link_program)(int prog);
-  IMPORT(void, shader_source)(int shd, const char * src, unsigned sz);
-  IMPORT(void, tex_image_2d)(int t, int lvl, int i_fmt, int w, int h, int border, int fmt, int type, const void * data, int sz);
-  IMPORT(void, tex_parameter_i)(int t, int p, int v);
-  IMPORT(int, uniform2f)(int u, float, float);
-  IMPORT(int, uniform1i)(int u, int);
-  IMPORT(int, use_program)(int prog);
-  IMPORT(void, vertex_attrib_array_pointer)(int idx, int qty, int type, bool norm, int stride, int offset);
-  IMPORT(void, viewport)(int x, int y, int w, int h);
-}
 
 static void shader(int prog, int type, jute::view src) {
   using namespace gelo;
